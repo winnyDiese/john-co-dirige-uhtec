25bd1293ae27ccc72e0c8d11edede928faedefed
winnyDiese <winnydiese@gmail.com>
2024-09-12 21:19:52 +0200

    lkjblkjblibli

--- src/app/destinataire/page.jsx
+++ src/app/destinataire/page.jsx
@@ -14,26 +14,30 @@ const Page = () => {
 
             <div className='px-32 py-20 pt-5'>
                 <h2 class="text-2xl font-bold mb-4">Destinataires</h2>
-                <div class="bg-white shadow-md rounded-lg p-6 mb-8">
-                    <h3 class="text-xl font-semibold mb-4">Ajouter un destinataire</h3>
-                    <form class="space-y-4">
-                    <div>
-                        <label for="nom" class="block text-sm font-medium text-gray-700">Nom</label>
-                        <input type="text" id="nom" v-model="nouveauDestinataire.nom" required class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-blue-500 focus:border-blue-500"/>
+                
+                <div className="px-32">
+                    <div class="bg-white shadow-md rounded-lg p-6 mb-8 mx-32">
+                        <h3 class="text-xl font-semibold mb-4">Ajouter un destinataire</h3>
+                        <form class="space-y-4">
+                        <div>
+                            <label for="nom" class="block text-sm font-medium text-gray-700">Nom</label>
+                            <input type="text" id="nom" v-model="nouveauDestinataire.nom" required class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-blue-500 focus:border-blue-500"/>
+                        </div>
+                        <div>
+                            <label for="email" class="block text-sm font-medium text-gray-700">Email</label>
+                            <input type="email" id="email" v-model="nouveauDestinataire.email" required class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-blue-500 focus:border-blue-500"/>
+                        </div>
+                        <div>
+                            <label for="telephone" class="block text-sm font-medium text-gray-700">Téléphone</label>
+                            <input type="tel" id="telephone" v-model="nouveauDestinataire.telephone" required class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-blue-500 focus:border-blue-500"/>
+                        </div>
+                        <button type="submit" class="w-full bg-blue-600 text-white py-2 px-4 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">
+                            Ajouter le destinataire
+                        </button>
+                        </form>
                     </div>
-                    <div>
-                        <label for="email" class="block text-sm font-medium text-gray-700">Email</label>
-                        <input type="email" id="email" v-model="nouveauDestinataire.email" required class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-blue-500 focus:border-blue-500"/>
-                    </div>
-                    <div>
-                        <label for="telephone" class="block text-sm font-medium text-gray-700">Téléphone</label>
-                        <input type="tel" id="telephone" v-model="nouveauDestinataire.telephone" required class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-blue-500 focus:border-blue-500"/>
-                    </div>
-                    <button type="submit" class="w-full bg-blue-600 text-white py-2 px-4 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">
-                        Ajouter le destinataire
-                    </button>
-                    </form>
                 </div>
+
                 <div>
                     <h3 class="text-xl font-semibold mb-2">Liste des destinataires</h3>
                     <div class="bg-white shadow-md rounded-lg overflow-hidden">
