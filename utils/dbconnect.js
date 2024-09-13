36f44bb7eae4751e7956ffb710aee8e388fffc0d
winnyDiese <winnydiese@gmail.com>
2024-09-13 09:16:30 +0200

    ljbkjuyujlylb

---
+++ utils/dbconnect.js
@@ -0,0 +1,36 @@
+// utils/dbConnect.js
+
+import mongoose from 'mongoose';
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+if (!MONGODB_URI) {
+  throw new Error('Veuillez définir la variable d\'environnement MONGODB_URI');
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function dbConnect() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+    };
+
+    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+      return mongoose;
+    });
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
+
+export default dbConnect;
