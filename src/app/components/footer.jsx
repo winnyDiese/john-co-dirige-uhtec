d6f6e14bc0d7ab63b5b009d6858b30bbc6b7c1dd
winnyDiese <winnydiese@gmail.com>
2024-09-13 20:37:11 +0200

    lkjblkjbmknj

---
+++ src/app/components/footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      <footer class="bg-gray-800 text-white text-center p-4">
+        <p>&copy; 2024 / Jony-App - Application de Gestion des Transactions. Tous droits réservés.</p>
+      </footer>
+    </div>
+  )
+}
+
+export default Footer
