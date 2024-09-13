aa0dde8d0353ae592311869da953876373e2892b
winnyDiese <winnydiese@gmail.com>
2024-09-13 12:29:59 +0200

    lkjbbiub

--- src/app/api/transaction/route.js
+++ src/app/api/transaction/route.js
@@ -1,12 +1,12 @@
 import dbConnect from '../../../../utils/dbConnect';
-import Transaction from '../../../../models/transaction.model'; // Replace with the correct path to your Transaction model
+import Transaction from '../../../../models/transaction.model'; // Remplacez par le chemin correct de votre modèle Transaction
 import { NextResponse } from 'next/server';
 
 await dbConnect();
 
 export async function GET() {
     try {
-        // Fetch all transactions from the database
+        // Récupérer toutes les transactions depuis la base de données
         const transactions = await Transaction.find().sort({ createdAt: 'desc' });
         return NextResponse.json({ transactions }, { status: 200 });
     } catch (error) {
@@ -17,17 +17,24 @@ export async function GET() {
 
 export async function POST(req) {
     try {
-        // Extract data from the request body
-        const { montant, date, description } = await req.json();
+        // Extraire les données du corps de la requête
+        const { montant, date, nomBeneficiaire, telBeneficiaire, nomClient, telClient, description } = await req.json();
 
-        // Create a new transaction with the received data
-        const newTransaction = await Transaction.create({ montant, date, description });
+        // Créer une nouvelle transaction avec les données reçues
+        const newTransaction = await Transaction.create({
+            montant,
+            date,
+            nomBeneficiaire,
+            telBeneficiaire,
+            nomClient,
+            telClient,
+            description,
+        });
 
-        // Respond with a success message and the newly created transaction
+        // Répondre avec un message de succès et la nouvelle transaction créée
         return NextResponse.json({ message: 'Transaction added successfully!', transaction: newTransaction }, { status: 201 });
     } catch (error) {
         console.error('Error adding transaction:', error);
         return NextResponse.json({ message: "Error adding transaction" }, { status: 500 });
     }
 }
-    
