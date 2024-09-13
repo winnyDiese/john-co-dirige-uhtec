6b7ae3ae76a99c5b17b572ffdd3be2d95097185c
winnyDiese <winnydiese@gmail.com>
2024-09-13 11:03:56 +0200

    lkjbkjhb

--- models/agent.model.js
+++ models/agent.model.js
@@ -1,32 +1,19 @@
-import dbConnect from '../../../../utils/dbConnect';
-import Agent from '../../../../models/agent.model'; // Replace with the correct path to your Agent model
-import { NextResponse } from 'next/server';
+import mongoose from 'mongoose';
 
-await dbConnect();
+const AgentSchema = new mongoose.Schema({
+    nom: {
+        type: String,
+        required: true,
+    },
+    telephone: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
 
-export async function GET() {
-    try {
-        // Fetch all agents from the database
-        const agents = await Agent.find().sort({ createdAt: 'desc' });
-        return NextResponse.json({ agents }, { status: 200 });
-    } catch (error) {
-        console.error('Error fetching agents:', error);
-        return NextResponse.json({ message: 'Error fetching agents' }, { status: 500 });
-    }
-}
-
-export async function POST(req) {
-    try {
-        // Extract data from the request body
-        const { nom, telephone } = await req.json();
-
-        // Create a new agent with the received data
-        const newAgent = await Agent.create({ nom, telephone });
-
-        // Respond with a success message and the newly created agent
-        return NextResponse.json({ message: 'Agent added successfully!', agent: newAgent }, { status: 201 });
-    } catch (error) {
-        console.error('Error adding agent:', error);
-        return NextResponse.json({ message: "Error adding agent" }, { status: 500 });
-    }
-}
+const Agent = mongoose.models.Agent || mongoose.model('Agent', AgentSchema);
+export default Agent;
