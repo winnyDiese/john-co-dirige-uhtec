2c2c3553fbea0417cf2c2f7fe8615475d5813167
winnyDiese <winnydiese@gmail.com>
2024-09-13 10:17:22 +0200

    kjhvkhjvkuyv

--- src/app/api/client/route.js
+++ src/app/api/client/route.js
@@ -11,7 +11,6 @@ export async function GET() {
     return NextResponse.json( {agents} , { status: 200 })
 }
 
-
 export async function POST(req) {
     try {
       // Extraction des données du body de la requête
