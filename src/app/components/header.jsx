9f264ab5dfe888131c5b42b8c5fb56c7d0aaacab
winnyDiese <winnydiese@gmail.com>
2024-09-25 19:33:08 +0200

    sldfkjvbu

--- src/app/components/header.jsx
+++ src/app/components/header.jsx
@@ -5,10 +5,7 @@ const Header = () => {
   return (
     <header class="bg-blue-500 text-white p-4">
         <div class="flex justify-between items-center">
-            <h1 class="text-2xl font-bold">Gestion des Transactions</h1>
-            {/* <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
-                Déconnexion
-            </button> */}
+            <h1 class="text-2xl font-bold"><Link href="/">Gestion des Transactions</Link></h1>
         </div>
         <nav class="mt-4" v-if="isAuthenticated text-sm">
             <Link href="/agent" class="mr-4 hover:underline">Agents</Link>
